24a4488fe02de485aa1f11b6dcfcc0bc2f1171d0
edhoblyn <edhoblyn@gmail.com>
2026-03-03 12:26:07 +0000

    made seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Clear database
+puts "Cleaning up database..."
+
+WatchSession.destroy_all
+
+# Create Watchsession
+puts "Creating watch sessions..."
+
+10.times do
+  WatchSession.create!(
+    title: "#{Faker::Movie.genre} Night",
+    created_at: Faker::Time.backward(days: 5)
+  )
+end
