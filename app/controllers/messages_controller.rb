9086d64c4ff28c6c1938869af373de292fe6c3d1
edhoblyn <edhoblyn@gmail.com>
2026-03-03 15:00:06 +0000

    Fix MessagesController autoloading

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,19 @@
-def create
-  @watch_session = current_user.watch_sessions.find(params[:watch_session_id])
-  @message = @watch_session.messages.build(message_params)
-  @message.role = "user"
+class MessagesController < ApplicationController
+  def create
+    @watch_session = current_user.watch_sessions.find(params[:watch_session_id])
+    @message = @watch_session.messages.build(message_params)
+    @message.role = "user"
 
-  if @message.save
-    redirect_to watch_session_path(@watch_session)
-  else
-    render "watch_sessions/show", status: :unprocessable_entity
+    if @message.save
+      redirect_to watch_session_path(@watch_session)
+    else
+      render "watch_sessions/show", status: :unprocessable_entity
+    end
   end
-end
 
-private
+  private
 
-def message_params
-  params.require(:message).permit(:content)
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
