7dc330f7e8606afa7cce80ad3071b3c167f2c7ea
bianca correia <bi-a3@hotmail.com>
2026-03-03 16:36:18 +0000

    create views & controllers for watch sessions

--- app/controllers/watch_sessions_controller.rb
+++ app/controllers/watch_sessions_controller.rb
@@ -1,5 +1,4 @@
 class WatchSessionsController < ApplicationController
-<<<<<<< HEAD
   def new
     @session = Session.new
   end
@@ -14,17 +13,15 @@ class WatchSessionsController < ApplicationController
     end
   end
 
-  private
-
-  def watch_session_params
-    params.require(:watch_session).permit(:title, :description, :movie_or_show_name, :start_time, :end_time)
-  end
-
-=======
   def show
     @watch_session = current_user.watch_sessions.find(params[:id])
     @messages = @watch_session.messages.order(:created_at)
     @message = Message.new
   end
->>>>>>> 9086d64c4ff28c6c1938869af373de292fe6c3d1
+
+  private
+
+  def watch_session_params
+    params.require(:watch_session).permit(:title, :description, :movie_or_show_name, :start_time, :end_time)
+  end
 end
