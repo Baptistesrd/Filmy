a6d8ede91fee1cac49181425d8b9ff281385479a
edhoblyn <edhoblyn@gmail.com>
2026-03-04 13:01:52 +0000

    fixed error with controller

--- app/controllers/watch_sessions_controller.rb
+++ app/controllers/watch_sessions_controller.rb
@@ -1,4 +1,5 @@
 class WatchSessionsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_watch_session, only: %i[show edit update destroy]
 
   def new
@@ -10,21 +11,37 @@ class WatchSessionsController < ApplicationController
     @watch_session.user = current_user
 
     if @watch_session.save
-      redirect_to @watch_session, notice: "Watch session was successfully created."
+      redirect_to watch_session_path(@watch_session), notice: "Watch session was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
-
   def show
-    @watch_session = WatchSession.find(params[:id])
     @message = Message.new
   end
 
+  def edit
+  end
+
+  def update
+    if @watch_session.update(watch_session_params)
+      redirect_to watch_session_path(@watch_session), notice: "Watch session was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @watch_session.destroy
+    redirect_to root_path, notice: "Watch session was successfully deleted."
+  end
+
   private
+
+  def set_watch_session
+    @watch_session = current_user.watch_sessions.find(params[:id])
+  end
 
   def watch_session_params
     params.require(:watch_session).permit(:title, :description, :genre, :mood, :movie_or_show_name)
