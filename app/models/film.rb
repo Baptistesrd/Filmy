3fa484ccffa3af7861ae9dd774f92b8a08b13116
edhoblyn <edhoblyn@gmail.com>
2026-03-06 13:04:27 +0000

    added working JustWatch Link

--- app/models/film.rb
+++ app/models/film.rb
@@ -2,4 +2,9 @@ class Film < ApplicationRecord
   belongs_to :watch_session
 
   validates :title, presence: true
+
+  def justwatch_url
+    slug = title.parameterize
+    "https://www.justwatch.com/uk/movie/#{slug}"
+  end
 end
